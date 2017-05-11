970f8ca7b159e0bed537f52aae492c346a4091b0
HSchone <pizzazzy7@gmail.com>
2017-05-11 12:28:02 -0600

    Completed new Swing GUI

--- src/Ninjitsu.java
+++ src/Ninjitsu.java
@@ -4,6 +4,9 @@ public class Ninjitsu {
 	public static void main(String[] args) {
 		System.out.println("Initial Ninja");
 		System.out.println("Experiment with a new, risky GUI");
+		
+		
+		System.out.println("New Swing GUI works perfectly! #MayTheForceBeWithYou");
 
 	}
 
