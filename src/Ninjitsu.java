b3c6deeb51ea371fb5535e83be99e994feb0f312
HSchone <pizzazzy7@gmail.com>
2017-05-11 12:36:11 -0600

    Fixing the reset problem

--- src/Ninjitsu.java
+++ src/Ninjitsu.java
@@ -4,9 +4,11 @@ public class Ninjitsu {
 	public static void main(String[] args) {
 		System.out.println("Initial Ninja");
 		System.out.println("Experiment with a new, risky GUI");
-		
+		System.out.println("Bug Fix 19");
+	
 		
 		System.out.println("New Swing GUI works perfectly! #MayTheForceBeWithYou");
+
 
 	}
 
